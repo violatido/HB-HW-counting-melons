a5b164108a55ced4931b37f9d52575284826fe34
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-01-29 15:51:48 +0000

    Python to JS translation

---
+++ counting_melons.js
@@ -0,0 +1,24 @@
+
+const melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+'Watermelon', 'Santa Claus', 'Casaba'];
+
+const countMelons = melonsList => {
+    let melonCounts = {};
+
+    const melonObj = melonsList.filter(val => 
+        val in melonCounts ? melonCounts[val] += 1 : melonCounts[val] = 1
+    );
+
+    return melonCounts;
+}
+
+console.log(countMelons(melonsToAdd));
